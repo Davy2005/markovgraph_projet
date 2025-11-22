4904a01ce5094fbdc6f3b38040d9924e28345011
salmaidrissi1 <elidrissidafalisalma@gmail.com>
2025-11-22 10:48:34 +0100

    Update utils.c
    démarrage de la partie 2

--- utils.c
+++ utils.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "utils.h"
+#include "hasse.h"
 
 char *getID(int i)
 {
@@ -28,72 +29,84 @@ char *getID(int i)
 }
 
 
-// Parti 1 
+// *******************
+// Partie 1
+// *******************
 
-// Fonction sur les cellules et listes
 
-// Créer une cellule (arête)
-t_cell *createCell(int sommetdest, float prob) {
-    t_cell *newCell = malloc(sizeof(t_cell));
-    if (newCell == NULL) {
-        perror("Erreur d’allocation mémoire !\n");
+
+// Création d'une cellule / arête
+cell *createCell(int destination, float prob) {
+
+    cell *newcell = malloc(sizeof(cell));
+    
+    if (newcell == NULL) {
+
+        printf("Erreur : Erreur d’allocation mémoire de la cellule !\n");
         exit(EXIT_FAILURE);
     }
-    newCell->sommetdest = sommetdest;
-    newCell->prob = prob;
-    newCell->next = NULL;
-    return newCell;
+
+    newcell->dest = destination;
+    newcell->proba = prob;
+    newcell->next = NULL;
+    return newcell;
+
 }
 
-// Créer une liste vide
-t_list createList() {
-    t_list list;
+
+
+// Créer une liste et initialisée à vide
+list createList() {
+    list list;
     list.head = NULL;
     return list;
 }
 
-// Ajouter une cellule à une liste (insertion en tête)
-void addCell(t_list *list, int sommetdest, float prob) {
-    t_cell *newCell = createCell(sommetdest, prob);
-    newCell->next = list->head;
-    list->head = newCell;
-}
-
 // Afficher une liste
-void printList(t_list *list) {
-    t_cell *cur = list->head;
-    while (cur != NULL) {
-        printf(" -> (%d, %f)", cur->sommetdest, cur->prob);
-        cur = cur->next;
+void displaylist(list *list) {
+    cell *courant = list->head;
+    while (courant != NULL) {
+        printf(" -> (%d, %f)", courant->dest, courant->proba);
+        courant = courant->next;
     }
     printf("\n");
 }
 
+// Insertion en tête de liste
+void addCellHead(list *list, int destination, float prob) {
+    cell *newCell = createCell(destination, prob);
+    newCell->next = list->head;
+    list->head = newCell;
+}
+
+
+
 // Créer une liste d’adjacence vide (tableau de listes)
-adjlist createAdjList(int size) {
-    adjlist adj;
-    adj.size = size;
-    adj.tab = malloc(size * sizeof(t_list));
-    if (adj.tab == NULL) {
-        perror("Erreur d’allocation mémoire !\n");
+adj_list createAdjaList(int size) {
+    adj_list adjlist;
+    adjlist.size = size;
+    adjlist.tab = malloc(size * sizeof(list));
+    if (adjlist.tab == NULL) {
+        printf("Erreur : Erreur d'allocation mémoire d'une liste adjacente !\n");
         exit(EXIT_FAILURE);
     }
 
     for (int i = 0; i < size; i++) {
-        adj.tab[i] = createList(); // chaque case contient une liste vide
+        adjlist.tab[i] = createList(); // les différentes indices ont maintenant une liste vide!
     }
 
-    return adj;
+    return adjlist;
 }
 
-void printAdjList(adjlist *adj) {
+
+
+void displayAdjaList(adj_list *adj) {
     for (int i = 0; i < adj->size; i++) {
         printf("Liste pour le sommet %d : [head @]", i + 1);
-        t_cell *cur = adj->tab[i].head;
+        cell *cur = adj->tab[i].head;
 
-        // Parcours de la liste chaînée du sommet i
         while (cur != NULL) {
-            printf(" -> (%d, %.2f)", cur->sommetdest, cur->prob);
+            printf(" -> (%d, %.2f)", cur->dest, cur->proba);
             cur = cur->next;
         }
 
@@ -101,12 +114,14 @@ void printAdjList(adjlist *adj) {
     }
 }
 
-adjlist readGraph(const char *filename) {
+
+
+adj_list readGraph(const char *filename) {
     FILE *file = fopen(filename, "rt"); // read-only, text
     int nbvert, depart, arrivee;
     float proba;
 
-    adjlist adj;  // déclaration de la variable pour la liste d’adjacence
+    adj_list adj;  // déclaration de la variable pour la liste d’adjacence
 
     if (file == NULL) {
         perror("Could not open file for reading");
@@ -119,13 +134,13 @@ adjlist readGraph(const char *filename) {
         exit(EXIT_FAILURE);
     }
 
-    adj = createAdjList(nbvert);  
+    adj = createAdjaList(nbvert);  
     // Initialiser une liste d’adjacence vide à partir du nombre de sommets
 
     while (fscanf(file, "%d %d %f", &depart, &arrivee, &proba) == 3) {
 
         // Ajouter l’arête avec conversion vers indices internes (0-based)
-        addCell(&adj.tab[depart-1], arrivee, proba);
+        addCellHead(&adj.tab[depart-1], arrivee, proba);
     }
 
     fclose(file);
@@ -133,63 +148,77 @@ adjlist readGraph(const char *filename) {
 }
 
 
-// Vérifier si la somme des probabilités sortantes d'un sommet est égal à 1
-// Dit si c'est bien un graphe de Markov
+// Check si la somme des probabilités sortantes d'un sommet est égal à 1 ( entre 0,99 et 1).
+// Retourne si c'est bien un graphe de Markov
 
 
-int verifGraphDeMarkov(adjlist *liste) {
+int verifMarkov(adj_list *liste) {
+
+    int markov = 1; // on suppose que tout est correct au début
     int i;
-    int estMarkov = 1; // on suppose que tout est correct au début
 
-    // Première boucle : on vérifie si tous les sommets sont corrects
+    // On vérifie si tous les sommets sont conformes
     for (i = 0; i < liste->size; i++) {
-        float somme = 0.0;
-        t_cell *cour = liste->tab[i].head;
 
+        float total = 0.0;
+        cell *cour = liste->tab[i].head;
+
+        
         while (cour != NULL) {
-            somme += cour->prob;
+
+            total += cour->proba;
             cour = cour->next;
+
         }
 
-        // somme doit être comprise entre 0.99 et 1.00
-        if (somme < 0.99 || somme > 1.0) {
-            estMarkov = 0;
+
+
+        // La somme des probas => doit être comprise entre 0.99 et 1.00
+        if (total < 0.99 || total > 1.0) {
+
+            markov = 0;
+
         }
     }
 
-    // Si tout est correct
-    if (estMarkov == 1) {
-        printf("Le graphe est un graphe de Markov\n");
+    // Si c'est bon alors on return 1 et on dit que c'est bien un graph de Markov
+    if (markov == 1) {
+
+        printf("Le graphe est bien un graphe de Markov !\n");
         return 1;
+
     }
 
-    // Sinon, message global
-    printf("Le graphe n'est pas un graphe de Markov\n");
+    // Sinon, on dit que c'est pas un graph de Markov et on affiche les causes
+    printf("Le graphe n'est pas un graphe de Markov !\n");
 
-    // Et on affiche les sommets problématiques
+    // Affichage des sommets qui posent problèmes
     for (i = 0; i < liste->size; i++) {
-        float somme = 0;
-        t_cell *cour = liste->tab[i].head;
+
+
+        float total = 0;
+        cell *cour = liste->tab[i].head;
 
         while (cour != NULL) {
-            somme += cour->prob;
+            total += cour->proba;
             cour = cour->next;
         }
 
-        if (somme < 0.99 || somme > 1.00) {
-            printf("la somme des probabilites du sommet %d est %f\n", i + 1, somme);
+        if (total < 0.99 || total > 1.00) {
+            printf("La somme de probabilite du sommet %d est de [%f].\n", i + 1, total);
         }
     }
 
     return 0;
 }
 
-// Fonction pour exporter un graphe au format Mermaid
-void creerMermaid(adjlist *g, const char *filename) {
+// Conversion d'un fichier de donnée de graph.txt en fichier de donnee mermaid.
 
-    FILE *f = fopen(filename, "w");
+void convertMermaid(adj_list *liste, const char *nomdufichier) {
+
+    FILE *f = fopen(nomdufichier, "w");
     if (f == NULL) {
-        printf("Erreur lors de la création du fichier mermaid\n");
+        printf("Erreur dans la conversion du fichier de donnee mermaid.\n");
         return;
     }
 
@@ -203,24 +232,31 @@ void creerMermaid(adjlist *g, const char *filename) {
     fprintf(f, "flowchart LR\n");
 
     // Sommets
-    for (int i = 0; i < g->size; i++) {
+    for (int i = 0; i < liste->size; i++) {
         fprintf(f, "%s((%d))\n", getID(i+1), i+1);
     }
 
     // Arêtes
-    for (int i = 0; i < g->size; i++) {
+    for (int j = 0; j < liste->size; j=j+1) {
 
-        t_cell *cour = g->tab[i].head;
+        cell *cour = liste->tab[j].head;
 
-        char sommetdepart[10];
-        strcpy(sommetdepart, getID(i+1)); // On est forcé à utiliser strcpy pour les 2 getID ( source et arrivé ) car getID fonctionne pas deux fois sur la même fonction, la valeur est écrasée.
+        char somdep[10];
+        strcpy(somdep, getID(j+1));
+
+        // On est forcé à utiliser strcpy pour les 2 getID ( source et arrivé ) 
+        // car getID fonctionne pas deux fois sur la même fonction, la valeur est écrasée à chaque fois.
+
 
         while (cour != NULL) {
 
-            char sommetarrivee[10];
-            strcpy(sommetarrivee, getID(cour->sommetdest));
 
-            fprintf(f, "%s -->|%.2f| %s\n", sommetdepart, cour->prob, sommetarrivee);
+            char somarv[10];
+
+
+            strcpy(somarv, getID(cour->dest));
+
+            fprintf(f, "%s -->|%.2f|%s\n", somdep, cour->proba, somarv);
 
             cour = cour->next;
         }
@@ -230,7 +266,6 @@ void creerMermaid(adjlist *g, const char *filename) {
 }
 
 
-
-
-
-
+// **************************************************************
+// PARTIE 2 – TARJAN
+// **************************************************************
