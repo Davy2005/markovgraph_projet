f0b3e74024a1951e6e0119970fdf3761a3e62107
Davy2005 <davyli.contact@gmail.com>
2025-11-22 18:53:40 +0100

    Add link structures and graph functions in hasse.h

--- hasse.h
+++ hasse.h
@@ -1,14 +1,33 @@
 #ifndef __HASSE_H__
 #define __HASSE_H__
+#include "utils.h"
 
-void removeTransitiveLinks(t_link_array *p_link_array);
+// Un lien entre deux classes (from -> to)
+typedef struct {
+    int from;
+    int to;
+} t_link;
 
-// Une cellule représente une arête sortante d’un sommet
-typedef struct cell {
-    int sommetdest;              // sommet d’arrivée
-    float prob;            // probabilité de passage
-    struct cell *next;     // pointeur vers la cellule suivante
-} t_cell;
+// Tableau dynamique de liens
+typedef struct {
+    t_link *links;
+    int log_size; // nombre de liens utilisés
+    int capacity; // capacité allouée
+} t_link_array;
+
+// Supprime les liens transitifs dans le Hasse ( Fonction déjà donné dans le sujet )
+void removeTransitiveLinks(t_link_array *);
+
+// Construit les liens entre classes à partir du graphe
+t_link_array buildClassLinks(adj_list *, tarjan_partition *);
+
+
+// Export du diagramme de Hasse en Mermaid
+void convertHasseToMermaid(tarjan_partition *, t_link_array *, const char *);
+
+// Affiche : transitoire/persistante, absorbant, irréductible
+void displayGraphSpecificity(tarjan_partition *, t_link_array *);
+
 
 /**
  * @brief Creates a link array from the given partition and graph.
